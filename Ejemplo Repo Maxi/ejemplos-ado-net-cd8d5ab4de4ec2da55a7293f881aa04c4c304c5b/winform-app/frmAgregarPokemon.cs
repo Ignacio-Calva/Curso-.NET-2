9308f215f88507c42e6432ef58ce094a8e637d4f
IgnacioCalva <ignaciocalva15@gmail.com>
2025-04-25 00:22:37 -0300

    Pruebas con proyecto Pokemon

---
+++ Ejemplo Repo Maxi/ejemplos-ado-net-cd8d5ab4de4ec2da55a7293f881aa04c4c304c5b/winform-app/frmAgregarPokemon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform_app
+{
+    public partial class frmAgregarPokemon : Form
+    {
+        frmPokemons formulario1;
+        public frmAgregarPokemon(frmPokemons formulario)
+        {
+            formulario1 = formulario;
+            InitializeComponent();
+        }
+
+        private void btnAgregarForm_Click(object sender, EventArgs e)
+        {
+            Pokemon pokemonAgregado = new Pokemon();
+            pokemonAgregado.Numero = int.Parse(tbNumero.Text); 
+            pokemonAgregado.Nombre = tbNombre.Text.Trim(); 
+            pokemonAgregado.Descripcion = tbDesc.Text.Trim(); 
+            PokemonNegocio negocio = new PokemonNegocio();
+            negocio.agregarPokemon(pokemonAgregado);
+            MessageBox.Show("Se ha agregado el pokemon.");
+        }
+
+        private void frmAgregarPokemon_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            formulario1.Show();
+        }
+    }
+}
---
+++ Ejemplo Repo Maxi/ejemplos-ado-net-cd8d5ab4de4ec2da55a7293f881aa04c4c304c5b/winform-app/frmPokemons.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform_app
+{
+    public partial class frmPokemons : Form
+    {
+        public frmPokemons()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPokemons_Load(object sender, EventArgs e)
+        {
+            PokemonNegocio negocio = new PokemonNegocio();
+            dgvPokemons.DataSource = negocio.listar();
+            cbSeleccion.DataSource = negocio.listarNombre();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            PokemonNegocio negocio = new PokemonNegocio();
+            negocio.sacarPokemon(cbSeleccion.SelectedItem.ToString());
+            string pokemonRemovido = "Se há removido el pokemon " + cbSeleccion.SelectedItem.ToString();
+            MessageBox.Show(pokemonRemovido);
+            dgvPokemons.DataSource = negocio.listar();
+            cbSeleccion.DataSource = negocio.listarNombre();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            frmAgregarPokemon formulario2 = new frmAgregarPokemon(this);
+            formulario2.FormClosed += frmAgregarPokemon_FormClosed;
+            this.Hide();
+            formulario2.ShowDialog();
+        }
+        private void frmAgregarPokemon_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // Esto es lo que querés que se ejecute al cerrar el segundo formulario
+            PokemonNegocio negocio = new PokemonNegocio();
+            dgvPokemons.DataSource = negocio.listar();
+            cbSeleccion.DataSource = negocio.listarNombre();
+        }
+
+    }
+}
